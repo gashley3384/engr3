c7f673395d575dbf7f7a167880f5c2b4976c7bab
gashley3384 <gashley3384@charlottesvilleschools.org>
2023-09-21 15:17:38 -0400

    added simpleio

--- ultrasonicsensor.py
+++ ultrasonicsensor.py
@@ -2,6 +2,7 @@ import time
 import board
 import adafruit_hcsr04
 import neopixel
+import simpleio
 
 NUMPIXELS = 1  # Update this to match the number of LEDs.
 BRIGHTNESS = 0.05 # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
@@ -17,3 +18,4 @@ while True:
     except RuntimeError:
         print("Retrying!")
     time.sleep(0.1)
+    
