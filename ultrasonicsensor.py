bbd81b8498613975853383b81b47a71237b34dd1
gashley3384 <gashley3384@charlottesvilleschools.org>
2023-09-21 15:26:40 -0400

    added simpleio

--- ultrasonicsensor.py
+++ ultrasonicsensor.py
@@ -2,7 +2,7 @@ import time
 import board
 import adafruit_hcsr04
 import neopixel
-import simpleio
+
 
 NUMPIXELS = 1  # Update this to match the number of LEDs.
 BRIGHTNESS = 0.05 # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
@@ -11,6 +11,8 @@ PIN = board.NEOPIXEL  # This is the default pin on the 5x5 NeoPixel Grid BFF.
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 
 pixels = neopixel.NeoPixel(PIN, NUMPIXELS, brightness=BRIGHTNESS, auto_write=False)
+
+colorvar = 5
 
 while True:
     try:
